8ad539b976243c9e0e8863871588ba0371c3fad5
Heidi Joutsijoki <heidi20151@student.hamk.fi>
2024-03-17 11:49:29 +0200

    Add Cypress test

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import express from 'express'
+const express = require('express')
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use('dist')
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
